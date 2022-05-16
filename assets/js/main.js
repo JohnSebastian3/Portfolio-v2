27f7d0142c9606f32d5f1b099d0d44da42326896
John Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-05-16 16:42:27 -0400

    Restructure project grid and add button to expand and collapse more projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,36 @@ const nameInput = document.querySelector('#name');
 const form = document.querySelector('#form')
 const resetButton = document.querySelector('#reset-button');
 
+const viewButton = document.querySelector('.view-more button');
+
 resetButton.addEventListener('click', resetForm);
 
+viewButton.addEventListener('click', expandProjects);
+let isExpanded = false;
 
 function resetForm() {
   form.reset();
   nameInput.blur();
 }
+
+function expandProjects() {
+  let projects = document.querySelectorAll('.secondary');
+  projects = Array.from(projects);
+  if(!isExpanded) {
+    projects.forEach(project => {
+      project.classList.remove('hidden');
+    });
+    viewButton.innerText = 'View Less';
+    isExpanded = true;
+  } else {
+    projects.forEach(project => {
+      project.classList.add('hidden');
+    });
+    viewButton.innerText = 'View More';
+    isExpanded = false;
+  }
+}
+
+
 
 
