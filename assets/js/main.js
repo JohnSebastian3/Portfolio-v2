37de0bf35639991dd72081d7cb57d3a1f20834fb
John Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-05-16 10:57:50 -0400

    Add new profile picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
+const nameInput = document.querySelector('#name');
+const form = document.querySelector('#form')
+const resetButton = document.querySelector('#reset-button');
+
+resetButton.addEventListener('click', resetForm);
+
+
+function resetForm() {
+  form.reset();
+  nameInput.blur();
+}
+
 
